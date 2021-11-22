000f2fa10bd02c1af84ef8ef704bd9804f557264
a-ulasevich <a.ulasevich@eklib.com>
2021-11-22 16:55:06 +0300

    Added message handlers

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ app.post('/webhook', (req, res) => {
 
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            let webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+
+            // Get the sender PSID
+            let senderPsid = webhookEvent.sender.id;
+            console.log('Sender PSID: ' + senderPsid);
+
+            if (webhookEvent.message) {
+                handleMessage(senderPsid, webhookEvent.message);
+            } else if (webhookEvent.postback) {
+                handlePostback(senderPsid, webhookEvent.postback);
+            }
         });
 
         res.status(200).send('EVENT_RECEIVED');
@@ -52,3 +62,94 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+function handleMessage(senderPsid, receivedMessage) {
+    let response;
+
+    // Checks if the message contains text
+    if (receivedMessage.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of your request to the Send API
+        response = {
+            'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+        };
+    } else if (receivedMessage.attachments) {
+
+        // Get the URL of the message attachment
+        let attachmentUrl = receivedMessage.attachments[0].payload.url;
+        response = {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [{
+                        'title': 'Is this the right picture?',
+                        'subtitle': 'Tap a button to answer.',
+                        'image_url': attachmentUrl,
+                        'buttons': [
+                            {
+                                'type': 'postback',
+                                'title': 'Yes!',
+                                'payload': 'yes',
+                            },
+                            {
+                                'type': 'postback',
+                                'title': 'No!',
+                                'payload': 'no',
+                            }
+                        ],
+                    }]
+                }
+            }
+        };
+    }
+
+    // Send the response message
+    callSendAPI(senderPsid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = receivedPostback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { 'text': 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { 'text': 'Oops, try sending another image.' };
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(senderPsid, response);
+}
+
+// Sends response messages via the Send API
+function callSendAPI(senderPsid, response) {
+
+    // The page access token we have generated in your app settings
+    const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+    // Construct the message body
+    let requestBody = {
+        'recipient': {
+            'id': senderPsid
+        },
+        'message': response
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+        'method': 'POST',
+        'json': requestBody
+    }, (err, _res, _body) => {
+        if (!err) {
+            console.log('Message sent!');
+        } else {
+            console.error('Unable to send message:' + err);
+        }
+    });
+}
